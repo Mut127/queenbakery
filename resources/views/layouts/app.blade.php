e674f19780930dfbcbf93b666ae9d15b999dd739
Mut127 <muthiakhanza034@gmail.com>
2024-11-05 22:05:41 +0700

    sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,129 +21,134 @@
     <link rel="stylesheet" href="/css/vertical-layout-light/style.css">
     <!-- endinject -->
     <link rel="shortcut icon" href="/images/favicon.png" />
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
 </head>
 
 <body class="parent" style="padding-top: 5%;">
     @include('partials.header')
-    <nav class="sidebar sidebar-offcanvas" id="sidebar">
-        <ul class="nav">
-            <li class="nav-item">
-                <a class="nav-link {{ request()->is('admin/dashboard') ? 'active' : '' }}"
-                    href="{{ route('admin.dashboard') }}">
-                    <i class="icon-grid menu-icon"></i>
-                    <span class="menu-title">Dashboard</span>
-                </a>
-            </li>
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="sidebar sidebar-offcanvas" id="sidebar">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link {{ request()->is('admin/dashboard') ? 'active' : '' }}"
+                            href="{{ route('admin.dashboard') }}">
+                            <i class="icon-grid menu-icon"></i>
+                            <span class="menu-title">Dashboard</span>
+                        </a>
+                    </li>
 
-            <li class="nav-item">
-                <a class="nav-link {{ Route::is('admin.profile') ? 'active' : '' }}"
-                    href="{{ route('admin.profile') }}">
-                    <i class="icon-layout menu-icon"></i>
-                    <span class="menu-title">Profile</span>
-                </a>
-            </li>
+                    <li class="nav-item">
+                        <a class="nav-link {{ Route::is('admin.profile') ? 'active' : '' }}"
+                            href="{{ route('admin.profile') }}">
+                            <i class="icon-layout menu-icon"></i>
+                            <span class="menu-title">Profile</span>
+                        </a>
+                    </li>
 
-            <li class="nav-item">
-                <a class="nav-link {{ request()->is('admin/user*') ? 'active' : '' }}" data-toggle="collapse"
-                    href="#form-elements" aria-expanded="false" aria-controls="form-elements">
-                    <i class="icon-columns menu-icon"></i>
-                    <span class="menu-title">Pegawai</span>
-                    <i class="menu-arrow"></i>
-                </a>
-                <div class="collapse {{ request()->is('admin/user*') ? 'show' : '' }}" id="form-elements">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"><a
-                                class="nav-link {{ request()->is('admin/user') ? 'active' : '' }}"
-                                href="{{ route('admin.user') }}">Pegawai List</a></li>
-                        <!-- Add more sub-menu items if needed -->
-                    </ul>
-                </div>
-            </li>
+                    <li class="nav-item">
+                        <a class="nav-link {{ request()->is('admin/user*') ? 'active' : '' }}" data-toggle="collapse"
+                            href="#form-elements" aria-expanded="false" aria-controls="form-elements">
+                            <i class="icon-columns menu-icon"></i>
+                            <span class="menu-title">Pegawai</span>
+                            <i class="menu-arrow"></i>
+                        </a>
+                        <div class="collapse {{ request()->is('admin/user*') ? 'show' : '' }}" id="form-elements">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item"><a
+                                        class="nav-link {{ request()->is('admin/user') ? 'active' : '' }}"
+                                        href="{{ route('admin.user') }}">Pegawai List</a></li>
+                                <!-- Add more sub-menu items if needed -->
+                            </ul>
+                        </div>
+                    </li>
 
-            <li class="nav-item">
-                <a class="nav-link" data-toggle="collapse" href="#charts" aria-expanded="false"
-                    aria-controls="charts">
-                    <i class="icon-bar-graph menu-icon"></i>
-                    <span class="menu-title">Absensi</span>
-                    <i class="menu-arrow"></i>
-                </a>
-                <div class="collapse" id="charts">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"> <a class="nav-link" href="pages/charts/chartjs.html">Absensi</a>
-                        </li>
-                    </ul>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="collapse" href="#charts" aria-expanded="false"
+                            aria-controls="charts">
+                            <i class="icon-bar-graph menu-icon"></i>
+                            <span class="menu-title">Absensi</span>
+                            <i class="menu-arrow"></i>
+                        </a>
+                        <div class="collapse" id="charts">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item"> <a class="nav-link" href="pages/charts/chartjs.html">Absensi</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="collapse" id="charts">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item"> <a class="nav-link" href="pages/charts/chartjs.html">Kehadiran</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="collapse" href="#tables" aria-expanded="false"
+                            aria-controls="tables">
+                            <i class="icon-grid-2 menu-icon"></i>
+                            <span class="menu-title">Rekrutmen</span>
+                            <i class="menu-arrow"></i>
+                        </a>
+                        <div class="collapse" id="tables">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item"> <a class="nav-link" href="pages/tables/basic-table.html">Kelola Pelamar</a></li>
+                            </ul>
+                        </div>
+                        <div class="collapse" id="tables">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item"> <a class="nav-link" href="pages/tables/basic-table.html">Kelola Lowongan</a></li>
+                            </ul>
+                        </div>
+                        <div class="collapse" id="tables">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item"> <a class="nav-link" href="pages/tables/basic-table.html">Kelola Nilai</a></li>
+                            </ul>
+                        </div>
+                        <div class="collapse" id="tables">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item"> <a class="nav-link" href="pages/tables/basic-table.html">kelola Pengumuman</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="collapse" href="#icons" aria-expanded="false"
+                            aria-controls="icons">
+                            <i class="icon-contract menu-icon"></i>
+                            <span class="menu-title">Cuti</span>
+                            <i class="menu-arrow"></i>
+                        </a>
+                        <div class="collapse" id="icons">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item"> <a class="nav-link" href="pages/icons/mdi.html">Input Cuti</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false"
+                            aria-controls="auth">
+                            <i class="icon-head menu-icon"></i>
+                            <span class="menu-title">Penilaian Kinerja</span>
+                            <i class="menu-arrow"></i>
+                        </a>
+                        <div class="collapse" id="auth">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Penilaian Kinerja </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+
+
+            <main class="flex-fill">
+                <div class="body-content">
+                    @yield('content')
                 </div>
-                <div class="collapse" id="charts">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"> <a class="nav-link" href="pages/charts/chartjs.html">Kehadiran</a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-toggle="collapse" href="#tables" aria-expanded="false"
-                    aria-controls="tables">
-                    <i class="icon-grid-2 menu-icon"></i>
-                    <span class="menu-title">Rekrutmen</span>
-                    <i class="menu-arrow"></i>
-                </a>
-                <div class="collapse" id="tables">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"> <a class="nav-link" href="pages/tables/basic-table.html">Kelola Pelamar</a></li>
-                    </ul>
-                </div>
-                <div class="collapse" id="tables">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"> <a class="nav-link" href="pages/tables/basic-table.html">Kelola Lowongan</a></li>
-                    </ul>
-                </div>
-                <div class="collapse" id="tables">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"> <a class="nav-link" href="pages/tables/basic-table.html">Kelola Nilai</a></li>
-                    </ul>
-                </div>
-                <div class="collapse" id="tables">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"> <a class="nav-link" href="pages/tables/basic-table.html">kelola Pengumuman</a></li>
-                    </ul>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-toggle="collapse" href="#icons" aria-expanded="false"
-                    aria-controls="icons">
-                    <i class="icon-contract menu-icon"></i>
-                    <span class="menu-title">Cuti</span>
-                    <i class="menu-arrow"></i>
-                </a>
-                <div class="collapse" id="icons">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"> <a class="nav-link" href="pages/icons/mdi.html">Input Cuti</a></li>
-                    </ul>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false"
-                    aria-controls="auth">
-                    <i class="icon-head menu-icon"></i>
-                    <span class="menu-title">Penilaian Kinerja</span>
-                    <i class="menu-arrow"></i>
-                </a>
-                <div class="collapse" id="auth">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Penilaian Kinerja </a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-        </ul>
-    </nav>
-    <main>
-        <div class="body-content">
-            @yield('content')
+            </main>
         </div>
-
-    </main>
-
+    </div>
     @include('partials.footer')
 
     <!-- plugins:js -->
